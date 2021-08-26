678cc47d81d903af5afdd9c6eab881e8668b0cff
Navezm <navez.martin@gmail.com>
2021-08-26 11:29:28 +0200

    ChildNode ok

--- main.js
+++ main.js
@@ -1,16 +1,62 @@
+String.prototype.ucFirst = function() {
+    return this.charAt(0).toUpperCase() + this.substr(1); 
+}
+
+HTMLElement.prototype.hide = function() {
+    this.style.display = "none";
+}
+
+HTMLElement.prototype.show = function() {
+    this.style.display = "block";
+}
+
+NodeList.prototype.showNode = function() {
+    for (let element of this) {
+        element.parentElement.nextElementSibling.style.display = "block";
+    }
+}
+
+NodeList.prototype.hideNode = function() {
+    for (let element of this) {
+        element.parentElement.nextElementSibling.style.display = "none";
+    }
+}
+
+let str = "bonjour";
+
+console.log(str.ucFirst());
+
 let menuChapitre = document.querySelectorAll(".menu_chapitre");
 let menuPoint = document.querySelectorAll(".menu_point");
 
-menuPoint.forEach((elt) => {
-    elt.style.display = "none";
-})
+// let hideElement = (elt) => {
+//     elt.style.display = "none";
+// }
 
-menuChapitre.forEach(element => {
-    element.addEventListener("click", function() {
-        if (this.nextSibling.nextElementSibling.style.display = "none") {
-            this.nextSibling.nextElementSibling.style.display = "block";
+// let showElement = (elt) => {
+//     elt.style.display = "block";
+// }
+
+for (let elt of menuPoint) {
+    elt.hide();
+}
+
+// for (let elt of menuChapitre) {
+//     elt.addEventListener("click", function(){
+//         if (this.nextElementSibling.style.display == "none") {
+//             this.nextElementSibling.show();
+//         } else {
+//             this.nextElementSibling.hide();
+//         }
+//     });
+// }
+
+for (let elt of menuChapitre) {
+    elt.addEventListener("click", function(){
+        if(this.nextElementSibling.style.display == "none") {
+            elt.childNodes.showNode();
         } else {
-            this.nextSibling.nextElementSibling.style.display = "none";
+            elt.childNodes.hideNode();
         }
     })
-});
+}
